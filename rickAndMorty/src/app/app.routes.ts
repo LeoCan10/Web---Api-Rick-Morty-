f900f2ba85a01eed1e6aee7c9422ec277d6e0558
Leonardo Cangialosi <leocangialosi04@gmail.com>
2025-09-16 17:25:27 -0300

    FK

--- rickAndMorty/src/app/app.routes.ts
+++ rickAndMorty/src/app/app.routes.ts
@@ -1,13 +1,8 @@
-// src/app/app.routes.ts
-import { RouterModule, Routes } from '@angular/router';
-import { NgModule} from '@angular/core';
-
+// ...existing code...
+import { Routes } from '@angular/router';
 import { CharactersComponent } from './pages/characters/characters.component';
 import { CharacterDetailComponent } from './pages/character-detail/character-detail.component';
 import { NotFoundComponent } from './pages/notfound/notfound.component';
-import { RenderMode } from '@angular/ssr';
-import { guestGuard } from './services/guest.guard';
-import { authGuard } from './services/auth.guard';
 
 export const routes: Routes = [
   {
@@ -16,32 +11,20 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-  path: 'characters/:id',
-  component: CharacterDetailComponent,
+    path: 'characters/:id',
+    component: CharacterDetailComponent,
   },
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'login',
+    redirectTo: 'characters',
   },
-  {
+    {
     path: 'notfound',
     component: NotFoundComponent,
   },
   {
     path: '**',
     redirectTo: 'notfound',
-  },
-  { path: 'login',
-    loadComponent: () => import('./components/login/login').then(m => m.LoginComponent),
-    canActivate: [guestGuard]
-  },
-  { path: 'register',
-    loadComponent: () => import('./components/register/register').then(m => m.RegisterComponent),
-    canActivate: [guestGuard]
-  },
-  { path: 'profile',
-    loadComponent: () => import('./components/profile/profile').then(m => m.ProfileComponent),
-    canActivate: [authGuard]
   },
 ];
--- rickAndMorty/src/app/components/navbar/navbar.component.ts
+++ rickAndMorty/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  imports: [RouterLinkActive, RouterLink]
+  imports: [
+    CommonModule,
+    RouterLinkActive,
+    RouterLink
+  ]
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(
+    private router: Router,
+    private snack: MatSnackBar
+  ) {}
+}
